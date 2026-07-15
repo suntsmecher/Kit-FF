bee599ca6e255ba6d08afc80bf2b8668b3f8eae6
BTZ <86950937+suntsmecher@users.noreply.github.com>
2026-07-15 23:13:18 +0300

    Update App.tsx

--- kitffa-web/src/App.tsx
+++ kitffa-web/src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Store from './pages/Store'
-import Dashboard from './pages/Dashboard'
-import Vote from './pages/Vote'
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
+import { Dashboard } from './pages/Dashboard';
+import { Vote } from './pages/Vote';
 import './App.css'
 
 function App() {
